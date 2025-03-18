433e1574c5690082a4633b0cf39fa2dc669165f0
Guilherme Dugaich <guidugaich.dev@gmail.com>
2025-03-18 11:02:59 -0300

    create repo for vector search

--- src/repository/listings.py
+++ src/repository/listings.py
@@ -1,10 +1,23 @@
 from typing import List, Any
 from pymongo import MongoClient
+from scripts.embed_database_one_off import embedding_model_id
 
 class ListingsRepository:
     def __init__(self, db: MongoClient):
-        self.db = db['sample_airbnb']['listingsAndReviews']
-
+        self.db = db
+        self.collection = self.db['sample_airbnb']['listingsAndReviews']
+    
     def get_matching_listings(self, embedded_query: str) -> List[Any]:
-        # print('will call vector db', embedded_query)
-        return []
+        results = self.collection.aggregate([
+            {
+                "$vectorSearch": {
+                    "queryVector": embedded_query,
+                    "path": "plot_embedding",
+                    "numCandidates": 1000,
+                    "limit": 10,
+                    "index": f"embedding_{embedding_model_id}_index"
+                }
+            }
+        ])
+        
+        return results
