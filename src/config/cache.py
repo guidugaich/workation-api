9e1669afe86ff9488ff65101695fec74674e7daa
Guilherme Dugaich <guidugaich.dev@gmail.com>
2025-04-04 16:24:57 -0300

    adjust clients

--- src/config/cache.py
+++ src/config/cache.py
@@ -10,11 +10,11 @@ def get_cache_client() -> redis.Redis:
     global _cache_client
     if _cache_client is None:
         print('starting Redis instance')
-        _db_client = redis.Redis(
+        _cache_client = redis.Redis(
             host=os.getenv("REDIS_HOST"),
-            port=19752,
+            port=os.getenv("REDIS_PORT"),
             decode_responses=True,
             username=os.getenv("REDIS_USERNAME"),
             password=os.getenv("REDIS_PASSWORD"),
         )
-    return _db_client
+    return _cache_client
