7d8aeb458645f747dd3336f8298086bfbf46497b
Gui Dugaich <guidugaich.dev@gmail.com>
2025-04-29 20:25:55 +0200

    add cors policy

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from src.router.recommendation import recommendation_router
@@ -10,6 +11,14 @@ def healthcheck() -> dict[str, str]:
     return {"status": "healthy"}
 
 app.include_router(recommendation_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
